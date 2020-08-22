1b3ccea7a7b16be8572dca6fb9a809af6cc6eba3
chbailey081 <39930155+chbailey081@users.noreply.github.com>
2020-08-21 20:24:15 -0400

    edit js

---
+++ dist/CritHit.dev.js
@@ -0,0 +1,17 @@
+"use strict";
+
+//Number between 1-20
+console.log(Math.floor(Math.random() * 20)) + 1; //Number between 1-10
+
+console.log(Math.floor(Math.random() * 10)) + 1; //Number between 1-8
+
+console.log(Math.floor(Math.random() * 8)) + 1; //Number between 1-6
+
+console.log(Math.floor(Math.random() * 6)) + 1; //Number between 1-4
+
+console.log(Math.floor(Math.random() * 4)) + 1; //Number between 1-100
+
+console.log(Math.floor(Math.random() * 100)) + 1; //let upper = 20
+//let lower = 1
+//let myRandom = Math.floor(Math.random() * (upper - lower + 1)) + lower
+//console.log(myRandom);
